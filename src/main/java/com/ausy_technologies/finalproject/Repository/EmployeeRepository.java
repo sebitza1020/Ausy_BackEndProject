55939cc74120c1022cf1a900b3d20722871db51f
sebitza1020 <munteansebastian@gmail.com>
2020-09-02 22:48:43 +0300

    getEmployeesOrderBy() methods implemented.

--- src/main/java/com/ausy_technologies/finalproject/Repository/EmployeeRepository.java
+++ src/main/java/com/ausy_technologies/finalproject/Repository/EmployeeRepository.java
@@ -4,7 +4,15 @@ import com.ausy_technologies.finalproject.Model.DAO.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     Employee findById(int id);
+
+    List<Employee> findAllByOrderByFirstName();
+
+    List<Employee> findAllByOrderByLastName();
+
+    List<Employee> findAllByOrderBySalary();
 }
